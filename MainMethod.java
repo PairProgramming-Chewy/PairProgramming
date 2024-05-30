c5c7b3f23745b584e774b8ef02161bbc664eb147
Chewie0633 <159922290+Chewie0633@users.noreply.github.com>
2024-05-30 00:48:46 -0400

    Update and rename MainMethod to MainMethod.java

--- MainMethod.java
+++ MainMethod.java
@@ -1,10 +1,12 @@
-
 import javax.swing.SwingUtilities;
 
 public class MainMethod {
     public static void main(String[] args) {
+        // Use SwingUtilities.invokeLater to ensure the GUI is created on the Event Dispatch Thread (EDT)
         SwingUtilities.invokeLater(() -> {
+            // Create an instance of DiceGameGUI
             DiceGameGUI game = new DiceGameGUI();
+            // Make the GUI visible
             game.setVisible(true);
         });
     }
